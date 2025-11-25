c32d380606b7f92185c39de425e7adeed5d60be6
felipemaifredo <felipemaifredo@gmail.com>
2025-11-24 22:23:23 -0300

    att

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,9 @@ if (started) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
